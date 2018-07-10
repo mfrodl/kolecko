76a90437e8e1037165b1993e8e10592c0ac8ee97
Martin Frodl <maarilainen@gmail.com>
2018-07-10 15:58:20 +0200

    Translate routes to Czech

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :teams, controllers: {
-    sessions: 'teams/sessions',
-    registrations: 'teams/registrations'
-  }
+  devise_for :teams,
+    path: '', path_names: {
+      sign_in: 'prihlaseni',
+      sign_out: 'odhlaseni',
+      sign_up: 'registrace',
+    },
+    controllers: {
+      sessions: 'teams/sessions',
+      registrations: 'teams/registrations'
+    }
 
   root 'main#index'
 
-  resources :teams
+  resources :teams, path: 'tymy'
 
   get '/pravidla', to: 'pages#show', as: :rules, page: 'rules'
   get '/vysledky', to: 'pages#show', as: :results, page: 'results'
