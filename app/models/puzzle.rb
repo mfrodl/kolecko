d0265eb8eb2701c947fcfcaf76168717f6848f35
Martin Frodl <maarilainen@gmail.com>
2019-01-06 23:54:21 +0100

    Add Puzzle.find_by_code method

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,10 +1,17 @@
 class Puzzle < ApplicationRecord
   has_many :answers, dependent: :destroy
-  before_save :normalize
+
+  before_save do
+    self.code = normalize(self.code)
+    self.solution = normalize(self.solution)
+  end
+
+  def self.find_by_code(code)
+    self.find_by(code: normalize(code))
+  end
 
   private
-    def normalize
-      self.code = I18n.transliterate(self.code).upcase
-      self.solution = I18n.transliterate(self.solution).upcase
+    def self.normalize(string)
+      I18n.transliterate(string).upcase
     end
 end
