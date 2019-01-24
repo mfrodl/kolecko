3b6bdf3f73445ddf5a5e37c9274aec2e22ff858a
Pavel Ondračka <pavel.ondracka@gmail.com>
2019-01-24 11:08:59 +0100

    Add few stub pages for the KoleCZko 2018
    The new pages are added at the top level, however it should probably
    have a separate subpage with its own logo, etc.
    E.g. something like /2018/sifry, /2018/vysledky instead of /sifry2018
    and its own styles. I'm not able to do it ATM so leaving for later...

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,9 @@ Rails.application.routes.draw do
 
   get '/pravidla', to: 'pages#show', as: :rules, page: 'rules'
   get '/vysledky', to: 'pages#show', as: :results, page: 'results'
+
+  get '/vysledky2018', to: 'pages#show', as: :results2018, page: 'results2018'
+  get '/tymy2018', to: 'pages#show', as: :teams2018, page: 'teams2018'
+  get '/pravidla2018', to: 'pages#show', as: :rules2018, page: 'rules2018'
+  get '/sifry2018', to: 'pages#show', as: :puzzles2018, page: 'puzzles2018'
 end
