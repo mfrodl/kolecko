da41f9a568b6349f9a08865ce8c434e334561dc0
Martin Frodl <maarilainen@gmail.com>
2019-02-09 00:24:11 +0100

    Optimize web for mobile browser
    This commit addresses most points raised in #8, including:
    
    * Text readable in both desktop and mobile browsers
    * Page content takes up the entire viewport (except for the top bar)
    * Menu hidden by default, toggled by the hamburger icon
    * Menu items spread far enough from one another to make clicking on
      touch screen comfortable
    * Nested menu structure

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,20 @@
+$(document).on('turbolinks:load', function() {
+  var Menu = function(element) {
+    this.element = element;
+
+    var menuItem = $('.menu-item', this.element);
+    menuItem.click(function() {
+      $(this).toggleClass('open');
+      $(this).next().slideToggle();
+    });
+  };
+
+  var menu = Menu($('.main-menu'));
+
+  var toggleMenu = function() {
+    $('.menu').toggleClass('open');
+    $('.hamburger').toggleClass('fa-bars fa-arrow-left');
+  };
+
+  $('.hamburger').click(toggleMenu);
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,20 @@ module ApplicationHelper
       render(partial: 'flash', locals: {type: type, message: message})
     end
   end
+
+  def menu_item(text, path, icon, **options)
+    render(partial: 'menu_item',
+           locals: {text: text, path: path, icon: icon, options: options})
+  end
+
+  def submenu(text, icon, &block)
+    block = capture(&block)
+    render(partial: 'submenu',
+           locals: {text: text, icon: icon, block: block})
+  end
+
+  def submenu_item(text, path, **options)
+    render(partial: 'submenu_item',
+           locals: {text: text, path: path, options: options})
+  end
 end
