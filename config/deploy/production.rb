eec7e9c3fa27d519afaa8c2807066979129bc26c
Tomáš Kuča <tomas@kuca.cz>
2019-05-14 00:57:01 +0200

    Deploy master branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-set :branch, 'deployment'
+set :branch, 'master'
 set :deploy_to, '/var/www/kolecko.sifrovacky.cz/'
 
 server 'localhost', user: 'gitlab-runner', roles: %w(web app)
