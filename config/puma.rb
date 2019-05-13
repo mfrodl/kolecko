b864a83e58cf05ebc1a30564a927fa786d7b19f2
Tomáš Kuča <tomas@kuca.cz>
2019-05-13 22:54:48 +0200

    Update puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,78 @@
+#!/usr/bin/env puma
+# frozen_string_literal: true
+
+require 'pathname'
+
+ROOT_DIR = Pathname.new(File.dirname(__FILE__) + '/../').realpath
+
+# The directory to operate out of.
+directory ROOT_DIR
+
+# Store the pid of the server in the file at "path".
+pidfile ROOT_DIR.join('tmp', 'pids').realpath.join('puma.pid')
+
+# Use "path" as the file to store the server info state. This is
+# used by "pumactl" to query and control the server.
+state_path ROOT_DIR.join('tmp', 'pids').realpath.join('puma.state')
+
+# Redirect STDOUT and STDERR to files specified. The 3rd parameter
+# ("append") specifies whether the output is appended, the default is
+# "false".
+#
+stdout_redirect ROOT_DIR.join('log').realpath.join('puma.stdout.log'),
+                ROOT_DIR.join('log').realpath.join('puma.stderr.log'), true
+
+# Bind the server to "url". "tcp://", "unix://" and "ssl://" are the only
+# accepted protocols.
+bind "unix://#{ROOT_DIR.join('tmp', 'sockets').realpath.join('puma.sock')}"
+
+# === Cluster mode ===
+
+# How many worker processes to run.  Typically this is set to
+# to the number of available cores.
+#
+# The default is "0".
+# workers 1
+
+# Allow workers to reload bundler context when master process is issued
+# a USR1 signal. This allows proper reloading of gems while the master
+# is preserved across a phased-restart. (incompatible with preload_app)
+# (off by default)
+
+prune_bundler
+
+# Additional text to display in process listing
+tag 'kolecko'
+
+# === Puma control rack application ===
+
+# Start the puma control rack application on "url". This application can
+# be communicated with to control the main server. Additionally, you can
+# provide an authentication token, so all requests to the control server
+# will need to include that token as a query parameter. This allows for
+# simple authentication.
+#
+# Check out https://github.com/puma/puma/blob/master/lib/puma/app/status.rb
+# to see what the app has available.
+
+activate_control_app "unix://#{ROOT_DIR.join('tmp', 'sockets').realpath.join('pumactl.sock')}", no_token: true
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
