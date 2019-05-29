e034a7ef7aea72c7460aeec3c2893388f1dbf0ea
Martin Frodl <maarilainen@gmail.com>
2019-05-30 00:26:14 +0200

    Redirect to team page after login by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :authenticate_team!
+
+  def signed_in_root_path(resource)
+    team_path(current_team)
+  end
 end
