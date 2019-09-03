3ae45cd3cec1c4f6450c46a6bdd4facc5e293534
Martin Frodl <maarilainen@gmail.com>
2019-09-03 06:10:13 +0200

    Revert "Actually check that we have the required points when creating hint request"
    This reverts commit e1257a2854fbcb6bbdf262ea02d23ed0054eb835.

--- app/models/hint_request.rb
+++ app/models/hint_request.rb
@@ -1,6 +1,6 @@
 class BalanceValidator < ActiveModel::Validator
   def validate(record)
-    if record.team.points < record.bounty
+    if record.team.points < 0
       message = "Pro tuto akci nemáte dostatek bodů " \
                 "(#{record.team.points_was} bodů k dispozici)"
       record.errors.add(:base, message)
