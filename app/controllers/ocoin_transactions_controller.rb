4da701a4cab7b5dbe406fcb465ab6183863836ae
Pavel Ondračka <pavel.ondracka@gmail.com>
2019-09-07 18:56:12 +0200

    Improve formating for the OCoin transctions list

--- app/controllers/ocoin_transactions_controller.rb
+++ app/controllers/ocoin_transactions_controller.rb
@@ -13,6 +13,12 @@ class OcoinTransactionsController < ApplicationController
 
   def index
     @points = current_team.points
-    @ot = OcoinTransaction.where(team: current_team).sort_by(&:created_at)
+    ot = OcoinTransaction.where(team: current_team).sort_by(&:created_at)
+    @ot = []
+    sum = 0
+    ot.each do |t|
+      sum += t.points
+      @ot << [t.created_at.strftime("%d.%m. %H:%M:%S"), t.points, sum, t.message]
+    end
   end
 end
